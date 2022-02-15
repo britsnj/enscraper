27e95cac3fe35def970e582a88c92154b925ae0e
Nico Brits <britsnj@gmail.com>
2022-02-15 18:38:21 +0000

    Regex search function playpen

---
+++ search_functions.py
@@ -0,0 +1,26 @@
+import docx
+import re
+from docx import Document
+
+document = docx.Document('data.docx')
+
+docdata = []
+for docpara in document.paragraphs:
+    docdata.append(docpara.text)
+
+docstring = ' ' .join(docdata)
+#print(docstring)
+
+
+std_list_raw = re.findall(r'[E][N]\s\d{3,8}', docstring)
+en_description_final =  []
+for item in std_list_raw:
+    en_description_final.append(item.strip())
+en_description_final = list(dict.fromkeys(en_description_final))
+print(en_description_final)
+
+#std_list_raw = re.findall(r'[I][E][C]\s\d{3,5}\s\-\s\w.+[^.]', docstring)
+#iec_description_final = []
+#for item in std_list_raw:
+#    iec_description_final.append(item.strip())
+#print(iec_description_final)
