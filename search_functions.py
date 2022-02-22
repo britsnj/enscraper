139d01f88ee4fcfe82010ad56b0be54e00612be2
Nico Brits <britsnj@gmail.com>
2022-02-22 22:42:11 +0000

    Regex WIP

--- search_functions.py
+++ search_functions.py
@@ -12,15 +12,15 @@ docstring = ' ' .join(docdata)
 #print(docstring)
 
 
-std_list_raw = re.findall(r'[E][N]\s\d{3,8}\s-[\s\w]*', docstring)
+std_list_raw = re.findall(r'[E][N]\s\d{3,8}\s-[\s\w]+(?=[\s.?!])[^.?!]*[.?!]', docstring)
 en_description_final =  []
 for item in std_list_raw:
     en_description_final.append(item.strip())
 en_description_final = list(dict.fromkeys(en_description_final))
 print(en_description_final)
 
-#std_list_raw = re.findall(r'[I][E][C]\s\d{3,5}\s\-\s\w.+[^.]', docstring)
-#iec_description_final = []
-#for item in std_list_raw:
-#    iec_description_final.append(item.strip())
-#print(iec_description_final)
+std_list_raw = re.findall(r'[^.?!]*(?<=[.?\s!])IEC(?=[\s.?!])[^.?!]*[.?!]', docstring)
+iec_description_final = []
+for item in std_list_raw:
+    iec_description_final.append(item.strip())
+print(iec_description_final)
