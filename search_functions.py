b7e604e48dd1c3ef59bc75081a736e5ffac02860
Nico Brits <britsnj@gmail.com>
2022-02-23 08:00:25 +0000

    Regex WIP

--- search_functions.py
+++ search_functions.py
@@ -12,15 +12,31 @@ docstring = ' ' .join(docdata)
 #print(docstring)
 
 
-std_list_raw = re.findall(r'[E][N]\s\d{3,8}\s-[\s\w]+(?=[\s.?!])[^.?!]*[.?!]', docstring)
-en_description_final =  []
-for item in std_list_raw:
-    en_description_final.append(item.strip())
-en_description_final = list(dict.fromkeys(en_description_final))
-print(en_description_final)
+#This section searches for the EN standard name and title
+#std_list_raw = re.findall(r'EN\s\d{3,8}\s-[\s\w]+(?=[\s.?!])[^.?!]*[.?!]', docstring)
+#en_description_final =  []
+#for item in std_list_raw:
+#    en_description_final.append(item.strip())
+#en_description_final = list(dict.fromkeys(en_description_final))
+#print(en_description_final)
 
-std_list_raw = re.findall(r'[^.?!]*(?<=[.?\s!])IEC(?=[\s.?!])[^.?!]*[.?!]', docstring)
-iec_description_final = []
-for item in std_list_raw:
-    iec_description_final.append(item.strip())
-print(iec_description_final)
+
+#This section searches for any sentence containgin the word IEC
+#std_list_raw = re.findall(r'[^.?!]*(?<=[.?\s!])IEC(?=[\s.?!])[^.?!]*[.?!]', docstring)
+#iec_description_final = []
+#for item in std_list_raw:
+#    iec_description_final.append(item.strip())
+#print(iec_description_final)
+
+
+#Function to search for standard based on standard prefix , EN, IEC, IS etc.
+
+def searchForStandard(stdName):
+    stdListRaw = re.findall(r''+stdName+'\s\d{3,8}\s-[\s\w]+(?=[\s.?!])[^.?!]*[.?!]', docstring)
+    stdDescriptionFinal =  []
+    for item in stdListRaw:
+        stdDescriptionFinal.append(item.strip())
+        stdDescriptionFinal = list(dict.fromkeys(stdDescriptionFinal))
+    print(stdDescriptionFinal)
+
+searchForStandard("EN")
