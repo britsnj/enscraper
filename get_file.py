3bf290a08a3697b60cf291ba6b537cadda3a05c5
Nico Brits <britsnj@gmail.com>
2022-03-01 22:14:19 +0000

    Regex WIP

--- get_file.py
+++ get_file.py
@@ -1,7 +1,15 @@
 import shutil
 
+
+#Ask user for filename.
+#This section will be refactored for TKinter
 directory = "C:\projects\enscraper\\"
-
-filepath = directory + input("Enter filename:")
-
+filename = input("Enter filename:")
+#Split filename and create a new backup filename with .bak extension
+name, ext = filename.rsplit('.', 1)
+backup_filename = '{}.{}'.format(name, 'bak')
+#Copy the file to data.docx
+filepath = directory + filename
 shutil.copyfile(filepath, directory+"data.docx")
+#Also copy file to backup file... for incase
+shutil.copyfile(filepath, directory + backup_filename)
--- ultimate_search.py
+++ ultimate_search.py
@@ -1,20 +1,39 @@
 import docx
 import re
 from docx import Document
-
+#Open the document as an object
 document = docx.Document('data.docx')
-
+#Create a file with the contents of the object by iterating through each paragraph and appending it to the docdata variable
 docdata = []
 for docpara in document.paragraphs:
     docdata.append(docpara.text)
-
+#Extract the text from the variable and save as a string.
 docstring = ' ' .join(docdata)
 
 print(docstring)
 
-stdName = "IS"
+#Declare global variables
+stdListFull = []
+stdListNames = []
 
-stdListRaw1 = re.findall(r''+stdName+'\s*E*N*\s*\d{2,8}:*\d*[+]*[A]*\d*:*\d*[+]*[A]*\d*:*\d*[+]*[A]*\d*:*\d*', docstring)
-stdListRaw = re.findall(r''+stdName+'\s*E*N*\s*\d{2,8}', docstring)
-print(stdListRaw1)
-print(stdListRaw)
+#Function to search for standard names based on standard input (i.e. EN, IEC, IEEE etc.
+def stdSearch(stdName):
+    #Find all standard names, including dates and appendixes and save to list.
+    stdListFull = re.findall(r''+stdName+'\s*E*N*\s*\d{2,8}:*\d*[+]*[A]*\d*:*\d*[+]*[A]*\d*:*\d*[+]*[A]*\d*:*\d*', docstring)
+    #Find only the standard names, excluding dates etc. and save to list.
+    stdListNames = re.findall(r''+stdName+'\s*E*N*\s*\d{2,8}', docstring)
+
+print(stdListFull)
+print(stdListNames)
+
+stdSearch("BS")
+globalStdNameList = stdListNames
+stdSearch("EN")
+globalStdNameList = globalStdNameList + stdListNames
+stdSearch("IEC")
+globalStdNameList = globalStdNameList + stdListNames
+stdSearch("IS")
+globalStdNameList = globalStdNameList + stdListNames
+stdSearch("I.S.")
+globalStdNameList = globalStdNameList + stdListNames
+print(globalStdNameList)
