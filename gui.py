54dc8a20387e997ccac6f4880f62df0a0d14ee93
Nico Brits <britsnj@gmail.com>
2022-03-04 07:56:27 +0000

    Regex WIP, Start on GUI

--- gui.py
+++ gui.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 #import themed widgets
 from tkinter import ttk
+from tkinter import messagebox
+#import file dialogue to be able to open file
+from tkinter import filedialog as fd
 # Create Window
 root = tk.Tk()
 #Rename window title
@@ -9,8 +12,42 @@ root.title('Document Standard Search')
 root.geometry('600x400+400+100')
 root.minsize(300, 150)
 
+#insert file open dialogue
+def select_file():
+    filetypes = (
+        ('word document', '*.docx'),
+        ('text files', '*.txt')
+    )
+
+    filename = fd.askopenfilename(
+        title='Open a file',
+        initialdir='/',
+        filetypes=filetypes
+    )
+
+    messagebox.showinfo(
+        title='Selected File',
+        message=filename
+    )
+
+open_button = ttk.Button(
+    root,
+    text='Select File',
+    command=select_file
+)
+
+open_button.pack()
 
 #Format here is widget= WidgetName(container, **Options)
+
+selectFile = ttk.Frame(root)
+selectFile.pack(padx=10, pady=10, fill='x', expand=True)
+filename_entry = ttk.Label(selectFile, text="Select File:")
+filename_entry.pack(fill='x', expand=True)
+
+
+
+
 message = ttk.Label(root, text="Hello World!")
 #Place the widget on the window
 message.pack()
