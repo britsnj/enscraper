b8aa07dc2b416f87c9c93402495f5eb174f44563
Nico Brits <britsnj@gmail.com>
2022-03-06 21:13:53 +0000

    Regex WIP, Start on GUI

--- gui.py
+++ gui.py
@@ -12,7 +12,10 @@ root.title('Document Standard Search')
 root.geometry('600x400+400+100')
 root.minsize(300, 150)
 
+selectfile = ttk.Frame(root)
+selectfile.pack(pady=10, padx=10, fill='x', expand=True)
 #insert file open dialogue
+
 def select_file():
     filetypes = (
         ('word document', '*.docx'),
@@ -29,28 +32,45 @@ def select_file():
         title='Selected File',
         message=filename
     )
+    return filename
 
 open_button = ttk.Button(
-    root,
+    selectfile,
     text='Select File',
     command=select_file
 )
 
-open_button.pack()
+
 
 #Format here is widget= WidgetName(container, **Options)
 
-selectFile = ttk.Frame(root)
-selectFile.pack(padx=10, pady=10, fill='x', expand=True)
-filename_entry = ttk.Label(selectFile, text="Select File:")
-filename_entry.pack(fill='x', expand=True)
+filename = tk.StringVar()
+
+def submit_clicked():
+    return filename
+
+
+#Filename
+
+filename_label = ttk.Label(selectfile, text="File to be scanned (complete path):")
+filename_label.pack(fill='x', expand=True)
+
+filename_entry = ttk.Entry(selectfile, textvariable=filename)
+filename_entry.pack(fill='x', expand=True,)
+
+#Submit button
+submit_button = ttk.Button(selectfile, text="Submit", command=submit_clicked)
+submit_button.pack(fill='x', expand=True, pady=10)
+
+text1 = ttk.Label(selectfile, text='Or click below to select a file')
+text1.pack(ipady=10, ipadx=10)
+
+open_button.pack()
 
 
 
 
-message = ttk.Label(root, text="Hello World!")
-#Place the widget on the window
-message.pack()
+
 
 
 
