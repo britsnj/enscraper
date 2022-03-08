7d7d90ac021da438852e8082ee786a82e9f25320
Nico Brits <britsnj@gmail.com>
2022-03-08 07:59:57 +0000

    Regex WIP, Start on GUI

--- get_file.py
+++ get_file.py
@@ -1,15 +1,23 @@
 import shutil
+import os.path
 
 
 #Ask user for filename.
 #This section will be refactored for TKinter
 directory = "C:\projects\enscraper\\"
 filename = input("Enter filename:")
-#Split filename and create a new backup filename with .bak extension
-name, ext = filename.rsplit('.', 1)
-backup_filename = '{}.{}'.format(name, 'bak')
-#Copy the file to data.docx
 filepath = directory + filename
-shutil.copyfile(filepath, directory+"data.docx")
-#Also copy file to backup file... for incase
-shutil.copyfile(filepath, directory + backup_filename)
+
+
+if os.path.exists(filepath):
+    #Split filename and create a new backup filename with .bak extension
+    name, ext = filename.rsplit('.', 1)
+    backup_filename = '{}.{}'.format(name, 'bak')
+    #Copy the file to data.docx
+    filepath = directory + filename
+    shutil.copyfile(filepath, directory+"data.docx")
+    #Also copy file to backup file... for incase
+    shutil.copyfile(filepath, directory + backup_filename)
+else:
+    no_file_msg = "That file does not exist. Please try again"
+    print(no_file_msg)
--- gui.py
+++ gui.py
@@ -14,6 +14,30 @@ root.minsize(300, 150)
 
 selectfile = ttk.Frame(root)
 selectfile.pack(pady=10, padx=10, fill='x', expand=True)
+
+#Format here is widget= WidgetName(container, **Options)
+
+filename = tk.StringVar()
+
+def submit_clicked():
+    return filename
+
+
+#Filename
+
+filename_label = ttk.Label(selectfile, text="File to be scanned (complete path):")
+filename_label.pack(fill='x', expand=True)
+
+filename_entry = ttk.Entry(selectfile, textvariable=filename)
+filename_entry.pack(fill='x', expand=True,)
+
+#Submit button
+submit_button = ttk.Button(selectfile, text="Submit", command=submit_clicked)
+submit_button.pack(fill='x', expand=True, pady=10)
+
+text1 = ttk.Label(selectfile, text='Or click below to select a file')
+text1.pack(ipady=10, ipadx=10)
+
 #insert file open dialogue
 
 def select_file():
@@ -41,29 +65,6 @@ open_button = ttk.Button(
 )
 
 
-
-#Format here is widget= WidgetName(container, **Options)
-
-filename = tk.StringVar()
-
-def submit_clicked():
-    return filename
-
-
-#Filename
-
-filename_label = ttk.Label(selectfile, text="File to be scanned (complete path):")
-filename_label.pack(fill='x', expand=True)
-
-filename_entry = ttk.Entry(selectfile, textvariable=filename)
-filename_entry.pack(fill='x', expand=True,)
-
-#Submit button
-submit_button = ttk.Button(selectfile, text="Submit", command=submit_clicked)
-submit_button.pack(fill='x', expand=True, pady=10)
-
-text1 = ttk.Label(selectfile, text='Or click below to select a file')
-text1.pack(ipady=10, ipadx=10)
 
 open_button.pack()
 
