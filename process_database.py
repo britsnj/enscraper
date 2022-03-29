710ce61ee82d222794843e6112331c3d2e32c68c
Nico Brits <britsnj@gmail.com>
2022-03-29 20:57:33 +0100

    Start working on database processing

--- process_database.py
+++ process_database.py
@@ -1,8 +1,12 @@
 import numpy as np
 import pandas as pd
+from tkinter import filedialog
+from tkinter import *
 
-# Input
-data_file = "db_files/20220325-211732.csv"
+root = Tk()
+root.withdraw()
+file_selected = filedialog.askopenfilename(title="Select file to merge with database", filetypes=(("csv files", "*.csv"),))
+print((file_selected))
 
 # Delimiter
 data_file_delimiter = ','
@@ -11,7 +15,7 @@ data_file_delimiter = ','
 largest_column_count = 0
 
 # Loop the data lines
-with open(data_file, 'r') as temp_f:
+with open(file_selected, 'r') as temp_f:
     # Read the lines
     lines = temp_f.readlines()
 
@@ -25,7 +29,7 @@ with open(data_file, 'r') as temp_f:
 # Generate column names (will be 0, 1, 2, ..., largest_column_count - 1)
 column_names = [i for i in range(0, largest_column_count)]
 # Read csv
-df = pd.read_csv(data_file, header=None, delimiter=data_file_delimiter, names=column_names)
+df = pd.read_csv(file_selected, header=None, delimiter=data_file_delimiter, names=column_names)
 df = df.transpose()
-df.dropna(df)
+print(df)
 
