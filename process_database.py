ba776fd56dca2cb563991b9c587587fbde2eae0f
Nico Brits <britsnj@gmail.com>
2022-03-30 21:29:02 +0100

    Start working on database processing

--- process_database.py
+++ process_database.py
@@ -31,5 +31,21 @@ column_names = [i for i in range(0, largest_column_count)]
 # Read csv
 df = pd.read_csv(file_selected, header=None, delimiter=data_file_delimiter, names=column_names)
 df = df.transpose()
+df = df.drop(0)
 print(df)
+header_values = df.columns.values
+
+new_df1 = df[0]
+new_df1 = new_df1.dropna()
+print(new_df1)
+new_df2 = df[1]
+new_df2 = new_df2.dropna()
+print(new_df2)
+final_list = pd.concat([new_df1, new_df2])
+final_list.columns = ["name",]
+final_list.head()
+final_list = final_list.drop_duplicates()
+#final_list = final_list.sort_values(['name'])
+final_list.reset_index(drop=True, inplace=True)
+print(final_list)
 
